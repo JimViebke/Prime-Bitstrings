59c3f7840c3dc8bc9946aac739fdc304083d0062
Jim Viebke <jviebke@gmail.com>
2022-06-18 00:44:54 -0400

    Optimizations and cleanup

--- Prime Bitstrings/main.cpp
+++ Prime Bitstrings/main.cpp
@@ -8,6 +8,7 @@
 #include <fstream>
 #include <sstream>
 #include <charconv>
+#include <bitset>
 
 #include "utility.hpp"
 #include "math.hpp"
@@ -38,10 +39,14 @@ void partial_sieve(const size_t& start, std::vector<uint8_t>& sieve)
 {
 	for (const size_t p : small_primes_lookup)
 	{
+		// only mark off the small primes not already here
+		if (p <= 17) continue;
+
 		// Find out how far past we are from the previous multiple of p.
 		// ie 3 == 10 % 7
 		// This will always be <= p.
-		size_t offset_from_last_pn = start % p;
+		size_t offset_from_last_pn = (start % p) / 2;
+		// Divide by 2 because the sieve only represents odd numbers
 
 		// Now mark false each multiple of p, starting from [0 + p - distance from previous p],
 		// where 0 is the n we started with.
@@ -52,11 +57,23 @@ void partial_sieve(const size_t& start, std::vector<uint8_t>& sieve)
 	}
 }
 
+const std::vector<uint8_t> generate_static_sieve()
+{
+	const std::vector<size_t> sieve_primes = { 3, 5, 7, 11, 13, 17 };
+
+	std::vector<uint8_t> sieve({ 3 * 5 * 7 * 11 * 13 * 17 }, true);
+
+	// for each prime, mark off all multiples
+	for (const size_t p : sieve_primes)
+		for (size_t i = p; i < sieve.size(); i += p)
+			sieve[i] = false;
+
+	return sieve;
+}
+
 void find_multibase_primes()
 {
 	gmp_random::r.seed(rand());
-
-	auto start = current_time_in_ms();
 
 	/*
 	Checkpoints to copy/paste as the starting point:
@@ -73,37 +90,46 @@ void find_multibase_primes()
 	size_t number = 0b1000000010000011110100010001000101001010110111001;
 
 	const size_t stopping_point = number + 500'000'000;
-	const size_t sieve_size = 1'000'000;
+	const std::vector<uint8_t> static_sieve = generate_static_sieve();
+	/* The number must start on an odd multiple of the sieve size.
+	 * To round N to the nearest odd multiple of K:
+	 * n -= k;
+	 * n -= n % 2k;
+	 * n += k; */
+	number -= static_sieve.size();
+	number -= number % (2 * static_sieve.size());
+	number += static_sieve.size();
+
+	// Don't start the clock until here
+	auto start = current_time_in_ms();
 
 	for (; number < stopping_point; )
 	{
-		std::vector<uint8_t> sieve(sieve_size, true);
+		// perform additional sieving on the static sieve
+		std::vector<uint8_t> sieve = static_sieve;
 		partial_sieve(number, sieve);
 
-		for (size_t i = 0; i < sieve_size; ++i, ++number)
+		for (size_t i = 0; i < static_sieve.size(); ++i, number += 2)
 		{
-			// Bail if this number is already known to have a factor < 1000
+			// Bail if this number is already known to have a prime factor < 1000
 			if (!sieve[i]) continue;
 
 			// Bail if n does not have a prime number of bits set.
 			if ((tiny_primes_lookup() & (1ull << pop_count(number))) == 0) continue;
-			// if we were to have tiny_primes be a 64-byte lookup instead of a 64-bit number:
+			// We could use a 64-byte lookup instead of 64-bit lookup:
 			// if (!tiny_primes[pop_count(number)]) continue;
 
 			// Bail if gcd(abs(# of even bits - # of odd bits), 1155) is not equal to one.
-			// Interestingly, this prevents us from detecting p7s, but not p8s+.
 			const int pca = (int)pop_count(number & 0xAAAAAAAAAAAAAAAA);
 			const int pcb = (int)pop_count(number & 0x5555555555555555);
 			if (gcd_1155[abs(pca - pcb)] != 1) continue;
+			// Instead of "discard if gcd( ... ) != 1", you can "discard the candidate if sa is not a prime greater than 12."
+			// These are effectively performance-indentical, as both are some_lookup[abs(pca - pcb)]
 
-			// Instead of "discard if gcd( ... ) != 1", you can "discard the candidate if sa is not a prime greater than 12." 
-			// These are effectively performance-indentical, as both involve some_lookup[ abs(pca - pcb) ].
-
-			// Bail if n is not prime in this base (base 2)
-			if (!mpir_is_prime(number, small_primes_cap)) continue; // can we do this natively without calling the lib?
-			// mpir_is_prime(number, small_primes_cap); // ~20 s
-			// misof_16k::is_prime(number); // ~50 s
-			// misof_262k::is_prime_2_64(number); // ~24 s
+			// Bail if n is not prime in base 2
+			if (!mpir_is_prime(number, small_primes_cap)) continue; // can we do this natively, ie, without calling the lib?
+			// misof_16k::is_prime(number); // ~2.5x slower
+			// misof_262k::is_prime_2_64(number); // ~1.25x slower
 
 			// convert uint64_t to char array of ['0', '1'...] for mpz_class
 			char bin_str[64 + 1];
@@ -131,9 +157,21 @@ void find_multibase_primes()
 	std::cout << "Finished. " << current_time_in_ms() - start << " ms elapsed\n";
 }
 
+void generate_permanent_sieve_sizes()
+{
+	size_t sieve_size = 1;
+	for (auto p : small_primes_lookup)
+	{
+		if (p == 2) continue;
+		sieve_size *= p;
+		std::cout << "Static sieve size would be size " << sieve_size << " using product of primes up to " << p << '\n';
+		if (sieve_size > 1'000'000'000) return;
+	}
+	std::cout << "(no suitable sieve size found)\n";
+}
+
+
 int main()
 {
 	find_multibase_primes();
-
-	// find_p2_8();
 }
