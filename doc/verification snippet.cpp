399eb309bf451a0e4a801fa80ef9aef9c5c7e552
Jim Viebke <jviebke@gmail.com>
2022-10-14 16:52:11 -0400

    Update verification snippet.cpp

--- doc/verification snippet.cpp
+++ doc/verification snippet.cpp
@@ -2,26 +2,24 @@
 #include <boost/multiprecision/miller_rabin.hpp>
 #include <iostream>
 
-using boost::multiprecision::cpp_int;
-using boost::multiprecision::pow;
+using namespace boost::multiprecision;
 
 cpp_int to_base(size_t n, size_t base)
 {
-	cpp_int number(0);
+	cpp_int number{0};
 	for (size_t i = 0; i < 64; ++i)
-		number += pow(cpp_int(base) * ((n >> i) & 1), i);
+		number += pow(cpp_int{base} * ((n >> i) & 1), i);
 	return number;
 }
 
 int main()
 {
-	using namespace boost::multiprecision;
-
 	const size_t bitstring = 0b1000000010000011110100010001000101001010110111001;
 
 	for (size_t base = 2;; ++base)
 	{
 		const cpp_int number = to_base(bitstring, base);
+		
 		if (miller_rabin_test(number, 25))
 		{
 			std::cout << "Prime in base " << base << ": " << number << '\n';
